f83811bc765e007dfca293588d1dd02517988d0c
v-giorgio7 <vgiorgio@ciandt.com>
2022-07-20 11:43:39 -0300

    update: pedidoBuilder remove warnings

--- 0x02/lanchonete/PedidoBuilder.java
+++ 0x02/lanchonete/PedidoBuilder.java
@@ -6,22 +6,22 @@ public class PedidoBuilder extends PedidoAbstractBuilder {
 
     @Override
     public void setLanche(TipoLanche tipo) {
-        pedido.adicionarItemDentroCaixa(new ItemPedido(TipoItemPedido.LANCHE, tipo));
+        pedido.adicionarItemDentroCaixa(new ItemPedido<>(TipoItemPedido.LANCHE, tipo));
     }
 
     @Override
     public void setBatata(TamanhoBatata tamanho) {
-        pedido.adicionarItemDentroCaixa(new ItemPedido(TipoItemPedido.BATATA, tamanho));
+        pedido.adicionarItemDentroCaixa(new ItemPedido<>(TipoItemPedido.BATATA, tamanho));
     }
 
     @Override
     public void setBrinde(TipoBrinde tipo) {
-        pedido.adicionarItemDentroCaixa(new ItemPedido(TipoItemPedido.BRINDE, tipo));
+        pedido.adicionarItemDentroCaixa(new ItemPedido<>(TipoItemPedido.BRINDE, tipo));
     }
 
     @Override
     public void setBebida(TipoBebida tipo) {
-        pedido.adicionarItemForaCaixa(new ItemPedido(TipoItemPedido.BEBIDA, tipo));
+        pedido.adicionarItemForaCaixa(new ItemPedido<>(TipoItemPedido.BEBIDA, tipo));
     }
 
     public Pedido build() {
